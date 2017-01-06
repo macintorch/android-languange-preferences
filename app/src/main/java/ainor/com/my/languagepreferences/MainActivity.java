2a2665929daf6eed2656f8ed50c898f2ce6cef89
Ainor Syahrizal <macintorch@gmail.com>
2017-01-06 09:13:30 +0800

    add shared preferences and text view

--- app/src/main/java/ainor/com/my/languagepreferences/MainActivity.java
+++ app/src/main/java/ainor/com/my/languagepreferences/MainActivity.java
@@ -1,15 +1,24 @@
 package ainor.com.my.languagepreferences;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void setLanguage(String language) {
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("package ainor.com.my.languagepreferences", Context.MODE_PRIVATE);
 
+        sharedPreferences.edit().putString("language", language).apply();
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        textView.setText(language);
 
     }
 
