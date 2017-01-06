26f493569a36d1fea6624a2ece155a63598f3fa1
Ainor Syahrizal <macintorch@gmail.com>
2017-01-06 09:10:46 +0800

    create alert dialog

--- app/src/main/java/ainor/com/my/languagepreferences/MainActivity.java
+++ app/src/main/java/ainor/com/my/languagepreferences/MainActivity.java
@@ -1,13 +1,41 @@
 package ainor.com.my.languagepreferences;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void setLanguage(String language) {
+
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_btn_speak_now)
+                .setTitle("Choose Language")
+                .setMessage("Which language would you like?")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        setLanguage("English");
+
+                    }
+                })
+                .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        setLanguage("Spanish");
+                    }
+                })
     }
 }
